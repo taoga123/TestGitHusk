1084c154753afb959d164796ed0b3478da1e5020
taoga123 <60774906+taoga123@users.noreply.github.com>
2020-05-27 17:09:16 +0700

    test git123

--- app/src/main/java/com/example/testgithusk/MainActivity.java
+++ app/src/main/java/com/example/testgithusk/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.testgithusk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
